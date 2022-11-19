06caf73de1ed82fa4b4d0fddbfe4089ad8d99915
Mikko Anttonen <mikkohj.anttonen@gmail.com>
2022-11-19 20:11:09 +0200

    Lisätty testausta

--- src/tests/random_dfs_test.py
+++ src/tests/random_dfs_test.py
@@ -8,6 +8,10 @@ class TestDFS(unittest.TestCase):
     def test_constructor_creates_size_correctly(self):
         self.assertEqual(self.dfs.size, 10)
 
+    def test_all_cells_visited_are_unique(self):
+        maze = self.dfs.create_maze(0,0)
+        self.assertEqual(len(self.dfs.visited),100)
+
     def test_correct_maze_length(self):
         maze = self.dfs.create_maze(0,0)
         self.assertEqual(len(maze), 100)
