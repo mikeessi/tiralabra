60bc9457f910f4a49b32c28df88b277111ba5b15
Mikko Anttonen <mikkohj.anttonen@gmail.com>
2022-12-01 04:20:46 +0200

    Siirretty käyttöliittymä kokonaan pygameen

--- src/draw.py
+++ src/draw.py
@@ -5,25 +5,26 @@ from clock import Clock
 from eventqueue import EventQueue
 from renderer import Renderer
 
-class App:
+class Draw:
     """
-    Luokka, joka mallintaa sokkelon piirtävää pygame-ikkunaa.
+    Luokka, joka mallintaa sokkelon piirtämistä.
     """
 
-    def __init__(self,route,size,cell_size):
+    def __init__(self,display,route,size):
         """
         Luo kello- tapahtumajono-, ja renderöintioliot.
-        Laskee ruudun koon sopivaksi s.e. se pysyy suurin piirtein
-        samana oli syötteen koko mikä tahansa.
+        Laskee solulle sopivan koon, jottei sokkelo mene muuta kuin suurilla
+        syötteillä ikkunan ulkopuolelle.
         Args:
+            display: pygame.display-olio, jolle kuva piirretään
             route: Lista ruudulle piirrettävistä soluista sopivassa järjestyksessä.
             size: Sokkelon reunan mitta soluina.
-            cell_size: Solun reunan pituus pikseleinä.
         """
+        self.display = display
         self.clock = Clock()
         self.eventqueue = EventQueue()
-        self.renderer = Renderer(cell_size)
-        self.screen_size = self.calculate_screen_size(size, cell_size)
+        self.cell_size = self.calculate_cell_size(size)
+        self.renderer = Renderer(self.cell_size)
         self.route = route
         self.route_so_far = []
 
@@ -32,7 +33,7 @@ class App:
         Käsittelee tapahtumat.
 
         Returns:
-            False, kun halutaan sulkea ikkuna.
+            False, kun halutaan palata takaisin menuun.
         """
         for event in self.eventqueue.get():
             if event.type == KEYDOWN and event.key == K_ESCAPE:
@@ -44,31 +45,26 @@ class App:
 
     def run(self):
         """
-        Hoitaa ikkunan käynnistyksen ja sisältää pygamen pääsilmukan.
+        Hoitaa sokkelon vaiheittaisen piirtämisen ruudulle.
         """
-        display = pygame.display.set_mode(self.screen_size)
-        pygame.display.set_caption("Sokkelo")
-        pygame.init()
         while True:
             if self.handle_events() is False:
                 break
             if len(self.route) > 0:
                 self.route_so_far.append(self.route.pop(0))
-            self.renderer.render(display, self.route_so_far)
+            self.renderer.render(self.display, self.route_so_far)
             self.clock.tick(30)
 
-        pygame.quit()
-        sys.exit()
 
-    def calculate_screen_size(self,size, cell_size):
+    def calculate_cell_size(self,size):
         """
-        Laskee ikkunalle sopivan koon solujen määrän ja koon perusteella.
-
+        Laskee solulle sopivan koon.
         Returns:
-            Tuple, jossa ikkunan koko pikseleinä
+            Solun koko.
         """
-        height = size*cell_size + size*2
-        return (height, height)
+        offset_total = size*2
+        cell_size = max(1,round((1000-offset_total)/size))
+        return cell_size
 
     def skip_to_end(self):
         self.route_so_far = self.route_so_far + self.route
---
+++ src/menu.py
@@ -0,0 +1,44 @@
+import pygame_menu
+from draw import Draw
+from algorithms.random_dfs import DFS
+from algorithms.wilson import Wilson
+from algorithms.kruskal import UF
+from parse_output import parse_kruskal_output, parse_wilson_output
+
+class Menu:
+
+    def __init__(self,display):
+        self.display = display
+
+        self.menu = pygame_menu.Menu("Sokkelosovellus",1000,1000, theme=pygame_menu.themes.THEME_DARK)
+        self.alg_input = self.menu.add.selector("Algoritmi :", [("Satunnaistettu Kruskal",1),("Satunnaistettu syvyyshaku",2),("Wilsonin algoritmi",3)])
+        self.size_input = self.menu.add.text_input("Sokkelon koko :", default="20",valid_chars=["1","2","3","4","5","6","7","8","9","0"])
+        self.menu.add.button("Luo sokkelo", self.create_maze)
+        self.menu.add.button("Poistu", pygame_menu.events.EXIT)
+
+    def create_maze(self):
+        size = self.get_size()
+        alg = self.alg_input.get_index()
+        if alg == 0:
+            uf = UF(size)
+            route = parse_kruskal_output(uf.kruskal())
+        if alg == 1:
+            dfs = DFS(size)
+            route = dfs.create_maze(0,0)
+        if alg == 2:
+            wilson = Wilson(size)
+            route = parse_wilson_output(wilson.create_maze())
+        self.draw_maze(route,size)
+
+    def get_size(self):
+        if self.size_input.get_value() == "":
+            size = 20
+        elif int(self.size_input.get_value()) == 0:
+            size = 20
+        else:
+            size = int(self.size_input.get_value())
+        return size
+
+    def draw_maze(self,route,size):
+        app = Draw(self.display,route, size)
+        app.run()
