bdfced2238db52b73cfdf81e6a736bc33d48b8a7
Mikko Anttonen <mikkohj.anttonen@gmail.com>
2022-12-07 13:48:41 +0200

    Päivitetty tasks.py

--- tasks.py
+++ tasks.py
@@ -4,3 +4,10 @@ from invoke import task
 def start(ctx):
     ctx.run("python src/index.py", pty=True)
 
+@task
+def lint(ctx):
+    ctx.run("pylint src/")
+
+@task
+def test(ctx):
+    ctx.run("pytest")
