9f48f02c30765424b633e3c72ae6c6e792284e21
Mikko Anttonen <mikkohj.anttonen@gmail.com>
2022-12-09 00:27:01 +0200

    Korjattu pylint issue

--- src/index.py
+++ src/index.py
@@ -2,6 +2,9 @@ import pygame
 from interface.menu import Menu
 
 def main():
+    """
+    Aloittaa pygame-instanssin ja luo siinä näytettävän menu-olion.
+    """
     display = pygame.display.set_mode((1000,1000))
     pygame.display.set_caption("Sokkelosovellus")
     pygame.init()
